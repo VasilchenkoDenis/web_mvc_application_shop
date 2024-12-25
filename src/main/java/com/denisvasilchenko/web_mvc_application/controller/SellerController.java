a9acafd9751cd4f75b8004b866b18d873498000c
Denis <virgilion1990@gmail.com>
2024-12-25 16:39:34 +0300

    feat: add SellerController with 3 methods

--- src/main/java/com/denisvasilchenko/web_mvc_application/controller/SellerController.java
+++ src/main/java/com/denisvasilchenko/web_mvc_application/controller/SellerController.java
@@ -1,7 +1,36 @@
 package com.denisvasilchenko.web_mvc_application.controller;
 
+import com.denisvasilchenko.web_mvc_application.entity.Product;
+import com.denisvasilchenko.web_mvc_application.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SellerController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/store")
+    public String showStore(Model model) {
+        model.addAttribute("products", productService.findAll());
+        return "store";
+    }
+
+    @GetMapping("/store/addProduct")
+    public String showAddProductForm() {
+        return "addProduct";
+    }
+
+    @PostMapping("/store/addProduct")
+    public String addProductToDataBase(@RequestParam String name, @RequestParam double price, @RequestParam double purchasePrice, @RequestParam int quantity) {
+        if(name!=null&&quantity!=0){
+            productService.saveProduct(new Product(name, price, purchasePrice, quantity));
+            return "redirect:/store";
+        }
+        return "redirect:/store/addProduct";
+    }
 }
