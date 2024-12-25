1614e9a09dcff0756c7e5a68103fdfd391bf2188
Denis <virgilion1990@gmail.com>
2024-12-25 16:40:17 +0300

    feat: add find method in ProductRepository

--- src/main/java/com/denisvasilchenko/web_mvc_application/repository/ProductRepository.java
+++ src/main/java/com/denisvasilchenko/web_mvc_application/repository/ProductRepository.java
@@ -3,6 +3,8 @@ package com.denisvasilchenko.web_mvc_application.repository;
 
 import com.denisvasilchenko.web_mvc_application.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Product findByNameAndPurchasePrice(String name, Double purchasePrice);
 }
