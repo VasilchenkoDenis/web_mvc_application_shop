64b51af1aef884338bf3a8b6435ff637e764a0a5
Denis <virgilion1990@gmail.com>
2024-12-27 18:16:47 +0300

    refactor: change imports

--- src/main/java/com/denisvasilchenko/web_mvc_application/service/SaleService.java
+++ src/main/java/com/denisvasilchenko/web_mvc_application/service/SaleService.java
@@ -2,7 +2,6 @@ package com.denisvasilchenko.web_mvc_application.service;
 
 import com.denisvasilchenko.web_mvc_application.entity.Product;
 import com.denisvasilchenko.web_mvc_application.entity.User;
-import com.denisvasilchenko.web_mvc_application.repository.ProductRepository;
 import com.denisvasilchenko.web_mvc_application.repository.SaleRepository;
 import com.denisvasilchenko.web_mvc_application.shop.Shop;
 import org.springframework.beans.factory.annotation.Autowired;
