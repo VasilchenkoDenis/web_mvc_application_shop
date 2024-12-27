4f44506c7964b57ee2f119f5c5c8fd5c5f08ee1c
Denis <virgilion1990@gmail.com>
2024-12-27 18:08:23 +0300

    refactor: delete unused imports

--- src/main/java/com/denisvasilchenko/web_mvc_application/MyUserDetails.java
+++ src/main/java/com/denisvasilchenko/web_mvc_application/MyUserDetails.java
@@ -1,19 +1,16 @@
 package com.denisvasilchenko.web_mvc_application;
 
-import com.denisvasilchenko.web_mvc_application.entity.Role;
 import com.denisvasilchenko.web_mvc_application.entity.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class MyUserDetails implements UserDetails {
-    private User user;
+    private final User user;
     public MyUserDetails(User user) {
         this.user = user;
     }
