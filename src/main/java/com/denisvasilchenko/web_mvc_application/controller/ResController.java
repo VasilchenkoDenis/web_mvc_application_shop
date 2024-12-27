385273a8475005e3dcdc335f8882ae1db3177105
Denis <virgilion1990@gmail.com>
2024-12-27 18:06:50 +0300

    feat: add new method (sellProduct)

--- src/main/java/com/denisvasilchenko/web_mvc_application/controller/ResController.java
+++ src/main/java/com/denisvasilchenko/web_mvc_application/controller/ResController.java
@@ -2,12 +2,18 @@ package com.denisvasilchenko.web_mvc_application.controller;
 
 
 import com.denisvasilchenko.web_mvc_application.entity.Product;
+import com.denisvasilchenko.web_mvc_application.entity.User;
 import com.denisvasilchenko.web_mvc_application.service.ProductService;
+import com.denisvasilchenko.web_mvc_application.service.SaleService;
+import com.denisvasilchenko.web_mvc_application.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.Authenticator;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,9 +21,31 @@ import java.util.List;
 public class ResController {
     @Autowired
     private ProductService productService;
+    @Autowired
+    private SaleService saleService;
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/store/search")
     public List<Product> search(@RequestParam String searchTerm) {
         return Collections.singletonList(productService.findProductByName(searchTerm));
+    }
+
+    @PostMapping("/store/sell")
+    public ResponseEntity<String> sellProduct(@RequestBody List<Product> products) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        User user = userService.findUserByName(username);
+        if (products==null || products.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Incorrect product data.");
+        }
+        try {
+            saleService.addSale(products, user);
+            return ResponseEntity.status(HttpStatus.OK).body("Sale added.");
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
     }
 }
