e93893f96c54aa3585bd3b705a44f2af12182bcf
Denis <virgilion1990@gmail.com>
2025-01-04 16:32:30 +0300

    feat: add new find method

--- src/main/java/com/denisvasilchenko/web_mvc_application/repository/SaleRepository.java
+++ src/main/java/com/denisvasilchenko/web_mvc_application/repository/SaleRepository.java
@@ -4,6 +4,10 @@ package com.denisvasilchenko.web_mvc_application.repository;
 import com.denisvasilchenko.web_mvc_application.entity.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    List<Sale> findAllBySaleDateTimeBetween(LocalDateTime from, LocalDateTime to);
 }
