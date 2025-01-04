3901acff4d90a4383b2587dab3580d59ba36c660
Denis <virgilion1990@gmail.com>
2025-01-04 16:31:09 +0300

    feat: add sale report functions

---
+++ src/main/java/com/denisvasilchenko/web_mvc_application/reports/SaleReport.java
@@ -0,0 +1,34 @@
+package com.denisvasilchenko.web_mvc_application.reports;
+
+import com.denisvasilchenko.web_mvc_application.entity.Product;
+import com.denisvasilchenko.web_mvc_application.entity.Sale;
+import com.denisvasilchenko.web_mvc_application.service.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+public class SaleReport {
+    private List<Sale> sales;
+    @Autowired
+    private SaleService saleService;
+
+    public SaleReport(List<Sale> sales) {
+        this.sales = sales;
+    }
+
+    public String getDailySalesAmount (){
+        List<Sale> sales = saleService.getSalesPerDay(LocalDate.now());
+        double total = 0;
+        for(Sale sale: sales){
+            List<Product> products = sale.getProducts();
+            for(Product product: products){
+                total+=product.getPrice();
+            }
+        }
+        return String.format("%.2f RUB", total);
+    }
+}
