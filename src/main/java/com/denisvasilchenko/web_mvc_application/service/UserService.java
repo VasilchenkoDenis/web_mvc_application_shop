643d4c5eb7dd67934e95e6e04bdbb8789ced99cd
Denis <virgilion1990@gmail.com>
2025-01-09 18:53:09 +0300

    refactor: code formatting

--- src/main/java/com/denisvasilchenko/web_mvc_application/service/UserService.java
+++ src/main/java/com/denisvasilchenko/web_mvc_application/service/UserService.java
@@ -1,7 +1,6 @@
 package com.denisvasilchenko.web_mvc_application.service;
 
 
-
 import com.denisvasilchenko.web_mvc_application.entity.User;
 import com.denisvasilchenko.web_mvc_application.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,28 +19,32 @@ public class UserService {
         Optional<User> user = userRepository.findById(id);
         return user.orElse(null);
     }
+
     public List<User> findAllUsers() {
         return userRepository.findAll();
     }
+
     public boolean saveUser(User user) {
         User savedUser = userRepository.findUserByName(user.getName());
         if (savedUser != null) {
             return false;
         }
 
-//        user.setRoles(Collections.singleton(Role.ROLE_SELLER));
         userRepository.save(user);
         return true;
     }
+
     public void deleteUser(int id) {
         Optional<User> user = userRepository.findById(id);
         if (user.isPresent()) {
             userRepository.deleteById(id);
         }
     }
+
     public void updateUser(User user) {
         userRepository.save(user);
     }
+
     public User findUserByName(String name) {
         return userRepository.findUserByName(name);
     }
