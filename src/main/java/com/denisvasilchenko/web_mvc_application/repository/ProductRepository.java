c0994c1d789cd32ca8582ae1f6d6b437c77b7197
Denis <virgilion1990@gmail.com>
2025-01-09 18:25:37 +0300

    feat: add method (findAllByAvailableTrue)

--- src/main/java/com/denisvasilchenko/web_mvc_application/repository/ProductRepository.java
+++ src/main/java/com/denisvasilchenko/web_mvc_application/repository/ProductRepository.java
@@ -3,9 +3,15 @@ package com.denisvasilchenko.web_mvc_application.repository;
 
 import com.denisvasilchenko.web_mvc_application.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Product findByNameAndPurchasePrice(String name, Double purchasePrice);
 
     Product findByName(String name);
+
+    List<Product> findAllByAvailableTrue();
 }
