857bb343432c7809c82b090d7a165efc89600e17
Denis <virgilion1990@gmail.com>
2025-01-09 18:52:53 +0300

    refactor: code formatting, delete unused imports

--- src/main/java/com/denisvasilchenko/web_mvc_application/repository/UserRepository.java
+++ src/main/java/com/denisvasilchenko/web_mvc_application/repository/UserRepository.java
@@ -3,12 +3,12 @@ package com.denisvasilchenko.web_mvc_application.repository;
 import com.denisvasilchenko.web_mvc_application.entity.User;
 import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findUserByName(@Size(min = 2, message = "Not less then 2 symbols") String name);
-
 
 
 }
