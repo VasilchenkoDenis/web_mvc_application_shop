3c8b6bf8263c8ae4c5ad5afdb5e44973e5fd946f
Denis <virgilion1990@gmail.com>
2025-01-09 19:03:04 +0300

    refactor: make isOpen field final

--- src/main/java/com/denisvasilchenko/web_mvc_application/shop/Shop.java
+++ src/main/java/com/denisvasilchenko/web_mvc_application/shop/Shop.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Component
 public class Shop {
 
-    private boolean isOpen = true;
+    private final boolean isOpen = true;
 
     @Autowired
     private ProductRepository productRepository;
