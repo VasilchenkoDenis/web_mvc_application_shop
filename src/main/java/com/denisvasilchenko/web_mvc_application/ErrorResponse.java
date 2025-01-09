3a3739f2cc9bd05fd21be5c53ff4e671af5c53c7
Denis <virgilion1990@gmail.com>
2025-01-09 18:56:44 +0300

    feat: add class for handling request errors

---
+++ src/main/java/com/denisvasilchenko/web_mvc_application/ErrorResponse.java
@@ -0,0 +1,31 @@
+package com.denisvasilchenko.web_mvc_application;
+
+import com.denisvasilchenko.web_mvc_application.entity.Product;
+
+import java.util.List;
+
+public class ErrorResponse {
+    private String message;
+    private List<Product> similarProducts;
+
+    public ErrorResponse(String message, List<Product> similarProducts) {
+        this.message = message;
+        this.similarProducts = similarProducts;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<Product> getSimilarProducts() {
+        return similarProducts;
+    }
+
+    public void setSimilarProducts(List<Product> similarProducts) {
+        this.similarProducts = similarProducts;
+    }
+}
