495a37f9daf2335626f526428a07bbad3fb78170
Denis <virgilion1990@gmail.com>
2025-01-09 18:22:45 +0300

    refactor: change method (addUserToDataBase)

--- src/main/java/com/denisvasilchenko/web_mvc_application/controller/AdminController.java
+++ src/main/java/com/denisvasilchenko/web_mvc_application/controller/AdminController.java
@@ -12,33 +12,41 @@ import org.springframework.web.bind.annotation.*;
 public class AdminController {
     @Autowired
     private UserService userService;
+
     @GetMapping("/admin")
     public String showAllUsers(Model model) {
         model.addAttribute("allUsers", userService.findAllUsers());
         return "admin";
     }
+
     @GetMapping("/admin/addUser")
     public String showAddUserForm() {
         return "addUser";
     }
+
     @PostMapping("/admin/addUser")
     public String addUserToDataBase(@RequestParam String username, @RequestParam String surname, @RequestParam String role, @RequestParam String password) {
-        if(username!=null&&surname!=null&&role!=null) {
-            if(userService.saveUser(new User(username, surname, role, password))){return "redirect:/admin";}
+        if (username != null && surname != null && role != null) {
+            if (userService.saveUser(new User(username, surname, role, password))) {
+                return "redirect:/admin";
+            }
         }
         return "redirect:/admin/addUser";
     }
+
     @PostMapping("/admin/delete/{id}")
     public String deleteUser(@PathVariable int id) {
         userService.deleteUser(id);
         return "redirect:/admin";
     }
+
     @PostMapping("/admin/updateUser/{id}")
     public String showFormToUpdate(@PathVariable int id, Model model) {
         User user = userService.findUserById(id);
         model.addAttribute("user", user);
         return "updateUser";
     }
+
     @PostMapping("/admin/updateUser")
     public String updateUser(@ModelAttribute("user") User user) {
         userService.updateUser(user);
