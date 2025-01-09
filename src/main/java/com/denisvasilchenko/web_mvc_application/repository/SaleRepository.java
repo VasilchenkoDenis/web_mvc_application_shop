ce8edd258e813f2583058abe15c9ff17c32ee2e1
Denis <virgilion1990@gmail.com>
2025-01-09 18:40:15 +0300

    feat: add method (findById)

--- src/main/java/com/denisvasilchenko/web_mvc_application/repository/SaleRepository.java
+++ src/main/java/com/denisvasilchenko/web_mvc_application/repository/SaleRepository.java
@@ -3,11 +3,15 @@ package com.denisvasilchenko.web_mvc_application.repository;
 
 import com.denisvasilchenko.web_mvc_application.entity.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
     List<Sale> findAllBySaleDateTimeBetween(LocalDateTime from, LocalDateTime to);
+
+    Sale findById(long id);
 }
