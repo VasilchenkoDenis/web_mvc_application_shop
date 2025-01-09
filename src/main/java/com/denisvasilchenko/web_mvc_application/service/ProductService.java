146ac32cc956fef5474b4698533175027545fba7
Denis <virgilion1990@gmail.com>
2025-01-09 18:35:02 +0300

    feat: add method (findSimilarProducts)

--- src/main/java/com/denisvasilchenko/web_mvc_application/service/ProductService.java
+++ src/main/java/com/denisvasilchenko/web_mvc_application/service/ProductService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.apache.commons.text.similarity.LevenshteinDistance;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,8 +16,9 @@ public class ProductService {
     private ProductRepository productRepository;
 
     public List<Product> findAll() {
-        return productRepository.findAll();
+        return productRepository.findAllByAvailableTrue();
     }
+
     public boolean saveProduct(Product product) {
         Product savedProduct = productRepository.findByNameAndPurchasePrice(product.getName(), product.getPurchasePrice());
         if (savedProduct != null) {
@@ -25,27 +27,38 @@ public class ProductService {
         productRepository.save(product);
         return true;
     }
+
     public Product findProductById(Long id) {
         Optional<Product> product = productRepository.findById(id);
         return product.orElse(null);
     }
 
-    public Product findProductByName(String name) {
+    public List<Product> findProductByName(String name) {
+        List<Product> foundProducts = new ArrayList<>();
         Product product = productRepository.findByName(name);
-        if (product!=null) {return product;}
+        if (product != null) {
+            foundProducts.add(product);
+        }
+
+        return foundProducts;
+    }
+
+    public List<Product> findSimilarProducts(String name) {
+        List<Product> similarProducts = new ArrayList<>();
         List<Product> products = productRepository.findAll();
         Product suitableProduct = null;
         int minDistance = Integer.MAX_VALUE;
-        for(Product p : products) {
+        for (Product p : products) {
             String str = p.getName();
             LevenshteinDistance levenshteinDistance = LevenshteinDistance.getDefaultInstance();
             int distance = levenshteinDistance.apply(str, name);
             System.out.println(distance);
             if (distance < minDistance) {
                 minDistance = distance;
-                suitableProduct=p;}
+                suitableProduct = p;
+            }
         }
-
-        return suitableProduct;
+        similarProducts.add(suitableProduct);
+        return similarProducts;
     }
 }
